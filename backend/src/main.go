999f75f510edc482b8ae5ebd43ac1b374795fed8
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2020-09-07 14:32:55 -0300

    Adding sample code of gorm (with errors)

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,7 +1,24 @@
 package main
 
 import (
-	"gorm.io/gorm"
 	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type Immobile struct {
+	gorm.Model
+	Id int
+}
+
+func main() {
+
+	dsn := "user= passwoord= dbname=test port=5432 sslmode=disable TimeZone=America/Sao_Paulo"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100})
+
+}
