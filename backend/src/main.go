52474a97a7c3ff0a298a92ca4d927c8a5fee2ddd
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2020-09-09 11:17:38 -0300

    Fix GORM errorsz

--- backend/src/main.go
+++ backend/src/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 
-//	"gorm.io/driver/postgres"
-//	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func main() {
-	fmt.Println("first")
+func main(){
+
+	fmt.Println("testing")
+	dsn := "user=postgres password=awesomepostgres dbname=test port=5432 sslmode=disable TimeZone=America/Sao_Paulo"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 }
