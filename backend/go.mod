e582f31ed2cec6eaf15d4f638b157f019f43eca9
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2020-09-23 09:29:54 -0300

    Fix GORM and add @VinGarcia adapter to the project

--- backend/go.mod
+++ backend/go.mod
@@ -8,8 +8,8 @@ require (
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/kr/pty v1.1.8 // indirect
 	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/vingarcia/go-adapter v0.0.0-20200922194846-6dfef361c712 // indirect
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
-	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
 	golang.org/x/tools v0.0.0-20191029190741-b9c20aec41a5 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gorm.io/driver/postgres v1.0.0
--- backend/src/main.go
+++ backend/src/main.go
@@ -4,50 +4,72 @@ import (
 	"GabrielNegreirosLima/telezap-imoveis/infra/env"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	"fmt"
+	"encoding/json"
+	routing "github.com/jackwhelpton/fasthttp-routing/v2"
+	"github.com/valyala/fasthttp"
+	adapter "github.com/vingarcia/go-adapter"
 )
 
 type Immobile struct {
 	gorm.Model
-	Price		uint
+	ID	int
+	Price	uint
 	QtdBedrooms	int
 	HaveCloset	bool `gorm:"not null"`
-	Area		float32 `gorm:"not null"`
+	Area	float32 `gorm:"not null"`
 	QtdCarspaces int
 	QtdRooms	int
 	QtdSuites	int
-	Summary		string
-	Address
+	Summary	string
+	AddressID	int
+	Address	Address
 }
 
 type House struct {
 	gorm.Model
-	Immobile Immobile	`gorm:"not null"`
+	ID	int
+	ImmobileID int
+	Immobile Immobile
 }
 
 type Apartment struct {
 	gorm.Model
-	Immobile Immobile `gorm:"not null"`
+	ID	int
 	Number	int
 	QtdDinerRooms	int
 	Floor	int
 	PriceCondominium	float32
 	HasDoorman	bool
+	ImmobileID int
+	Immobile Immobile
 }
 
 
 type Neighborhood struct {
 	gorm.Model
+	ID	int
 	Name	string `gorm:"not null"`
 }
 
 type Address struct {
 	gorm.Model
+	ID	int
 	Street	string `gorm:"not null"`
 	Number	int `gorm:"not null"`
 	Neighborhood Neighborhood
+	NeighborhoodID int
 }
 
+type Foo struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
+type MyType struct {
+	Value string
+}
 
 func main() {
 
@@ -64,8 +86,17 @@ func main() {
 		panic(err)
 	}
 
-	firHouse := House{
-		Immobile: Immobile {
+	// Migrate the schema
+	db.AutoMigrate(&House{})
+	db.AutoMigrate(&Apartment{})
+	db.AutoMigrate(&Immobile{})
+	db.AutoMigrate(&Neighborhood{})
+	db.AutoMigrate(&Address{})
+
+	var firHouse House
+    firHouse = House{
+		ID: 0,
+		Immobile:	Immobile {
 			Price: 1100,
 			QtdBedrooms: 2,
 			HaveCloset: true,
@@ -84,13 +115,36 @@ func main() {
 		},
 	}
 
-	// Migrate the schema
-	db.AutoMigrate(&House{})
-	db.AutoMigrate(&Apartment{})
-	db.AutoMigrate(&Immobile{})
-	db.AutoMigrate(&Neighborhood{})
-	db.AutoMigrate(&Address{})
-
 	// Create
 	db.Create(&firHouse)
+
+
+	router := routing.New()
+
+	router.Post("/adapted/<id>", adapter.Adapt(func(ctx *routing.Context, args struct {
+		ID       uint64 `path:"id"`
+		Brand    string `header:"brand,optional"`
+		Qparam   string `query:"qparam,required"`
+		//MyType   MyType `uservalue:"my_type"`
+		JSONBody Foo
+	}) error {
+		jsonResp, _ := json.Marshal(map[string]interface{}{
+			"ID":        args.ID,
+			"Brand":     args.Brand,
+			"Query":     args.Qparam,
+			"Body":      args.JSONBody,
+		//	"UserValue": args.MyType,
+		})
+		fmt.Println(string(jsonResp))
+		ctx.SetBody(jsonResp)
+
+		return nil
+	}))
+
+	port := "8765"
+	// Serve Start
+	fmt.Println("listening-and-serve", "server listening at:", port)
+	if err := fasthttp.ListenAndServe(":"+port, router.HandleRequest); err != nil {
+		fmt.Println("error-serving", err.Error())
+	}
 }
